44d08b8261183efedcaba113ef2bf40097e7915e
Dave Raggett <dsr@w3.org>
2020-05-31 18:37:45 +0100

    Improved UI

--- demos/decision-tree/decision.js
+++ demos/decision-tree/decision.js
@@ -2,57 +2,80 @@
 	Decision tree demo for Chunks rule language
 */
 
-window.addEventListener("load", function () {test.start(); }, false);
+window.addEventListener("load", test, false);
 
-let test = {
-	start: function () {
-		let startButton = document.getElementById("start");
-		let nextButton = document.getElementById("next");
-		let rule_engine = new RuleEngine();
-		startButton.disabled = true;
-		nextButton.disabled = true;
-		
-		startButton.onclick = function () {
-			nextButton.disabled = false;
-			let initial_goal = 'golf {\n' +
-		    '  state start\n ' +
-			'  outlook ' + test.randomChoice(["sunny", "cloudy", "rainy"]) + '\n ' +
-			'  humidity ' + test.randomIntFromInterval(20,80) + '\n ' +
-			'  windy ' + test.randomChoice(["true", "false"]) + '\n ' +
-			'}';
-
-			rule_engine.start(test.rules, test.facts, initial_goal);
-			return false;
-		}
-	
-		nextButton.onclick = function () {
-			rule_engine.next(test.rules, test.facts);
-			return false;
-		}
-		
-		let do_test = function (n) {
-			test.goals = new ChunkGraph();
-			fetch("rules.chk")
-			.then((response) => response.text())
-			.then(function (source) {
-				test.rules = new ChunkGraph(source);
-				let pre = document.getElementById("rules");
-				pre.innerText = source;
-				startButton.disabled = false;
-				
-				let a5 = test.rules.chunks['a5'];
-			});
-		};
-		
-		do_test();
-	},
-	randomIntFromInterval: function (min, max) {
+function test () {
+	function randomIntFromInterval (min, max) {
   		return Math.floor(Math.random() * (max - min + 1)) + min;
-  	},
-  	randomChoice: function (range) {
+  	}
+  	
+  	function randomChoice (range) {
 		// range must be an array of values with length > 1
-		let i = test.randomIntFromInterval(0, range.length - 1);
+		let i = randomIntFromInterval(0, range.length - 1);
 		return range[i];
-	},
+	}
+
+	let goalBuffer = document.getElementById("goalBuffer");
+	let factsBuffer = document.getElementById("factsBuffer");
+	let outputView = document.getElementById("output");
+	let ruleView = document.getElementById("rule");
+	let startButton = document.getElementById("start");
+	let nextButton = document.getElementById("next");
+	let log = new Log('log');
+	let ruleEngine = new RuleEngine();
+	let goal, rules, output;
 	
+	output = ruleEngine.addModule('output', new ChunkGraph(), {
+		log: function (action, bindings) {
+			outputView.innerText = bindings.value;
+		}
+	});
+	goal = ruleEngine.addModule('goal', new ChunkGraph());
+
+	startButton.disabled = true;
+	nextButton.disabled = true;
+	
+	function showBuffers () {
+		goalBuffer.innerText = goal.readBuffer();
+	}
+
+	startButton.onclick = function () {
+		log.clear();
+		nextButton.disabled = false;
+		ruleView.innerText = outputView.innerText = "";
+		let initial_goal = 'golf {\n' +
+		'  state start\n ' +
+		'  outlook ' + randomChoice(["sunny", "cloudy", "rainy"]) + '\n ' +
+		'  humidity ' + randomIntFromInterval(20,80) + '\n ' +
+		'  windy ' + randomChoice(["true", "false"]) + '\n ' +
+		'}';
+
+		ruleEngine.setGoal(initial_goal);
+		showBuffers();
+		return false;
+	}
+
+	nextButton.onclick = function () {
+		let text = "";
+		let match = ruleEngine.next();
+		showBuffers();
+		
+		if (match) {
+			let rule = match[0];
+			ruleView.innerText = rules.graph.ruleToString(rule);
+		} else
+			ruleView.innerText = "*** no matching rules ***"
+
+		return false;
+	}
+
+	fetch("rules.chk")
+	.then((response) => response.text())
+	.then(function (source) {
+		rules = ruleEngine.addModule('rules', new ChunkGraph(source));
+		let pre = document.getElementById("rules");
+		pre.innerText = source;
+		startButton.disabled = false;
+		console.log('' + ruleEngine.getModule('rules').graph);
+	});	
 };
