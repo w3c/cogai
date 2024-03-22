10b2f9ad41478636398c472f038101b2d46ad906
Dave Raggett <dsr@w3.org>
2024-03-22 11:58:54 +0000

    latest version

--- agents/agent.py
+++ agents/agent.py
@@ -1,6 +1,5 @@
 # cognitive agent for research on language, memory, reasoning and learning
 # initial focus on developing and testing language encoder and decoder
-# encoder uses 
 
 import torch, torchtext, nltk, re, random, math
 import torch.nn as nn
@@ -12,18 +11,19 @@ from collections import Counter, OrderedDict
 # define the hyper parameters
 
 vocab_size      = 0         # initialised on training data
-window_size     = 5         # sliding window on input tokens
-back_blend      = 0.5       # feedbackward blend coefficient
+seq_len         = 64        # maximum sentence length
+feedback        = 0         # feedbackward blend factor
 d_model         = 512       # model's vector dimension
 num_heads       = 4         # number of attention heads
 d_hidden        = 2048      # dimension of non linear layer
 dropout         = 0.1       # fraction of entries to zero
 num_blocks      = 4         # number of transformer blocks
 learning_rate   = 1e-3      # initial learning rate (was 1e-4)
-epochs          = 50        # number of training epochs
+epochs          = 500       # number of training epochs
 gen_interval    = 5         # epochs per generated example
 eval_interval   = 10        # epochs per validation loss
 
+assert 0 <= feedback and feedback < 1, "feedback coefficient must between zero and one"
 assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
 
 # manage tensors on best device
@@ -70,13 +70,15 @@ for sample in samples:
 # samples are now ready, so time to create vocabulary
 
 unk_token = '<unk>'
+eos_token = '<eos>'
 words = [word for sample in samples for word in sample]
-vocab = torchtext.vocab.vocab(OrderedDict([(word, 1) for word in words]), specials=[unk_token])
+vocab = torchtext.vocab.vocab(OrderedDict([(word, 1) for word in words]), specials=[unk_token, eos_token])
 
 vocab.set_default_index(-1)
-#make default index same as index of unk_token
+# make default index same as index of unk_token
 vocab.set_default_index(vocab[unk_token])
 print(f"vocab['out of vocab'] = {vocab['out of vocab']}; vocab['<unk>'] = {vocab['<unk>']}")
+print(f"vocab['<eos>'] = {vocab['<eos>']}")
 
 vocab_size = len(vocab)  # size of vocabulary
 
@@ -110,8 +112,8 @@ class Decoder(nn.Module):
     def __init__(self):
         super(Decoder, self).__init__()
         self.blocks = nn.ModuleList([
-        Transformer(d_model, num_heads, d_hidden, dropout)
-            for _ in range(num_blocks)
+            Transformer(d_model, num_heads, d_hidden, dropout)
+                for _ in range(num_blocks)
         ])
         self.linear = nn.Linear(d_model, vocab_size)
         self.softmax = nn.LogSoftmax(dim=-1)
@@ -125,28 +127,17 @@ class Decoder(nn.Module):
     def forward(self, x):
         for i in range(num_blocks):
             block = self.blocks[i] 
-            x = block(x,self.generate_mask(x.size(0)))
+            x = block(x, None)
+            #x = block(x,self.generate_mask(x.size(0)))
         x = self.linear(x)
         return self.softmax(x)
         
-    def firstPosition(self, working_memory):
-        # set working memory to first position
-        self.memory = working_memory
-        assert False, "firstPosition is not implemented"
-
-    def nextPosition(self, ):
-        # set working memory to next position
-        assert False, "nextPosition is not implemented"
-        
     def generate(self, working_memory):
-        self.firstPosition(working_memory)
         for _ in range(max_new_tokens):            
             logits = self(working_memory) #call forward without targets
-            probs = F.softmax(logits, dim=-1)
+            probs = self.softmax(x)
             data_next = torch.multinomial(probs, num_samples=1)
             data = torch.cat([data, data_next], dim=-1)
-            if not self.nextPosition():
-                break
         return data
 
 # absolute positional encoding for initial experiments
@@ -155,17 +146,11 @@ class Decoder(nn.Module):
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=5000):
         super(PositionalEncoding, self).__init__()
-
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        print(f"position shape is {position.shape}")
-
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        print(f"pe shape is {pe.shape}")
-        
         self.register_buffer("pe", pe)
     
     def forward(self, x):
@@ -177,27 +162,21 @@ class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
         self.embedding = nn.Embedding(vocab_size, d_model)
-        self.pos_encoder = PositionalEncoding(d_model, 10)
-        self.clear_retained()
+        self.pos_encoder = PositionalEncoding(d_model, seq_len)
+        #self.clear_retained()
         self.blocks = nn.ModuleList([
         Transformer(d_model, num_heads, d_hidden, dropout)
             for _ in range(num_blocks)
         ])
     
     def forward(self, x):
-        print(f"input shape is {x.shape}")
         x = self.embedding(x)
-        print(f"embedding1 shape is {x.shape}")
         x = self.pos_encoder(x)
-        print(f"embedding2 shape is {x.shape}")
-        quit()
         for i in range(num_blocks):
             block = self.blocks[i]
-            retained = self.retained[i]
-            if retained is not None:
-                x = x*(1-back_blend) + retained*back_blend
-            #x = block(x, None)
-            self.retained[i] = x
+            x = (1-feedback)*x + feedback*self.retained[i]
+            x = block(x, None)
+            self.retained[i] = x.clone()       
         return x
     
     # encoder output holds latent semantics
@@ -205,7 +184,22 @@ class Encoder(nn.Module):
         return self.retained[num_blocks-1]
 
     def clear_retained(self):
-        self.retained = [None] * num_blocks
+        self.retained = [torch.zeros((seq_len, d_model), dtype=torch.long)] * num_blocks
+        
+# pad line and replace words with tokens for tensor
+def prepare_sample(sample):
+    words = sample.copy()
+    if len(words) > seq_len:
+        words = text[0,seq_len]
+    if len(words) < seq_len:
+        words.append("<eos>")      
+    while len(words) < seq_len:
+        words.append(" ")
+    data = torch.tensor(vocab(words), dtype=torch.long)
+    data.detach()
+    target = data.clone()
+    target.detach()
+    return data, target
 
 # Cognitive agent with encoder and decoder
 
@@ -217,26 +211,14 @@ class Agent(nn.Module):
         self.train()
         
     def learn(self, sample):
-        print(f"learn on sample: {sample}")
         self.encoder.clear_retained() # clear retained outputs
-        print(f"working memory is {self.encoder.output()}")
-        for i in range(len(sample)):
-            # initialise sliding token window with window_size entries
-            # this mimics size constraints of human phonological loop
-            window = [None] * window_size
-            for j in range(window_size):
-                if 0 <= i+j and i+j < len(sample):
-                    window[j] = sample[i+j]
-            tokens = torch.tensor(vocab(sample), dtype=torch.long)
-            self.encoder.forward(tokens)
-        self.working_memory = self.encoder.output()
-        print(f"working memory is {self.working_memory}")
-        print(f"working memory shape is {self.working_memory.shape}")
-        quit()
-        self.decoder.firstPosition(self.working_memory)
-        for i in range(len(sample)):
-            logits = self.decoder.forward(encoder.output())
-            self.decoder.nextPosition()
+        data, target = prepare_sample(sample)
+        optimizer.zero_grad()
+        self.working_memory = self.encoder.forward(data)
+        logits = self.decoder.forward(self.working_memory)
+        loss = criterion(logits, target)
+        loss.backward()
+        optimizer.step()
         return loss
     
     def clear_retained():
@@ -244,14 +226,22 @@ class Agent(nn.Module):
             
 agent = Agent() # create instance of Agent class
 agent.train() # set it to training mode, use agent.eval() to disable dropouts
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(agent.parameters(), lr=0.001)
+
+def count_parameters(model):
+	return sum(p.numel() for p in agent.parameters() if p.requires_grad)
+
+print(f"\nThe agent model has {count_parameters(agent):,} trainable parameters\n")
+
 
 # loop over epochs, adjusting the learning rate as we go
 
 for epoch in range(epochs):
     best_loss = float('inf')
     for sample in samples:
+        agent.zero_grad()
         loss = agent.learn(sample)
         if loss < best_loss:
             best_loss = loss
     print(f"epoch {epoch+1} best loss {best_loss}")
-    
